8fc3b6ee7b768ca4866cdc784f4f9d0f22439ebb
Tim Watson <tiwatson@gmail.com>
2017-07-08 17:06:10 -0600

    BF

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,8 +1,11 @@
 class AvailabilitiesController < ApplicationController
   skip_before_action :login_required, only: [:import]
 
+  def show
+  end
+
   def import
-    Resque.enqueue(ImportAvailabilities::Index, facility.id, params[:import], params[:hash])
+    Resque.enqueue(AvailabilityImports::Index, params[:facility_id], params[:import], params[:hash])
     render status_code: 200
   end
 end
