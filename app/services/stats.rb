930729ff69521b8198c1317fac17a04f8aaae219
Tim Watson <tiwatson@gmail.com>
2017-07-24 20:22:33 -0600

    Stats/Health check

---
+++ app/services/stats.rb
@@ -0,0 +1,15 @@
+class Stats
+  @queue = :common
+  def self.perform
+    notifier = Slack::Notifier.new ENV['SLACK_HOOK'], channel: "#stats", username: "statBot"
+    notifier.ping output
+  end
+
+  def self.output
+    <<-EOS
+AR: #{AvailabilityRequest.active.count} / #{AvailabilityRequest.where('created_at > ?', 1.day.ago).active.count}/ #{AvailabilityRequest.where('created_at > ?', 1.hour.ago).active.count}
+AM: #{AvailabilityMatch.count} / #{AvailabilityMatch.where('created_at > ?', 1.day.ago).count}/ #{AvailabilityMatch.where('created_at > ?', 1.hour.ago).count}
+F: #{Facility.active_facilities.count}
+    EOS
+  end
+end
